4d36a8f661cafa876dab345555f68aedbc93224f
MertJohn <106982410+MertJohn@users.noreply.github.com>
2022-08-31 00:29:46 -0400

    Update ChipPayments.java

--- src/payments/ChipPayments.java
+++ src/payments/ChipPayments.java
@@ -3,7 +3,12 @@ package payments;
 public class ChipPayments {
     //chip in
     //processing payment
+    
     //payment successful
+    
     //done
+    
     //payment done
+    
+    //payment done2
 }
